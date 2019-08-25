63f4c67206d4fbe6f918764d6a57752e09a3ee12
Jeannne Dukunde <dukundem90@gmail.com>
2019-08-25 18:19:59 +0200

    style the project

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,10 +1,12 @@
 export class Quote {
    
     showDescription:boolean;
+    showAuthor:boolean;
     showUpvote:number;
     showDownVote:number;
     constructor(public name:string, public author:string, public description:string, public completeDate:Date,public upvote:number,public downvote:number){
             this.showDescription=false;
+            this.showAuthor=false;
             this.showUpvote= upvote || 0;
             this.showDownVote=downvote || 0;
     }
